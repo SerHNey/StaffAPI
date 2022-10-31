d2161e6143dc3b64e3386f9da3eb7e219276eeb8
Нечто Страшное <naumov-1212@Mail.ru>
2022-10-31 18:14:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/staffapi/DataModal.java
@@ -0,0 +1,36 @@
+package com.example.staffapi;
+
+public class DataModal {
+    private String name;
+    private String email;
+    private String phone;
+
+    public DataModal(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
